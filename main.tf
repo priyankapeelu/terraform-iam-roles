dd1f8fcfa564d0a52567e80c6118443e0773fed4
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-09 06:22:46 +0530

    Update terraform/terraform-iam-roles/

--- main.tf
+++ main.tf
@@ -31,3 +31,30 @@ resource "aws_iam_policy" "policy" {
     ]
   })
 }
+
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2_role_for_${var.ENV}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    Name = "ec2_role_for_${var.ENV}"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "test-attach" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.policy.arn
+}
